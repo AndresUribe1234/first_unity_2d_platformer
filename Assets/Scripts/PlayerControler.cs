a56a7c33be75e911de34111ef9c6dbc397816eac
Andres Uribe <anduri1997@gmail.com>
2023-08-15 08:32:35 -0500

    checkpoint with animation

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,9 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControler : MonoBehaviour
 {
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+    public bool IsMoving { get {
+            return _isMoving;
+        } 
+        private set {
+            _isMoving = value;
+            animator.SetBool("IsMoving",value);
+
+        } 
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+
+    public bool IsRunning { get {
+            return _isRunning;
+        } 
+        private set {
+            _isRunning = value;
+            animator.SetBool("IsRunning",value);
+
+        } 
+    }
+
+
+
+    private void Awake()
+    {
+        rb= GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +55,30 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+ 
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x*walkSpeed,rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+
+        if (context.started)
+        {
+            IsRunning = true;
+        } else if (context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 }
